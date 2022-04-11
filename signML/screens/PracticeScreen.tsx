ac7388154a71170705998cc6a5a007263dd61a43
kealymmb <kealymmb@gmail.com>
2022-04-11 22:28:47 +0200

    Added animation for current letter

--- signML/screens/PracticeScreen.tsx
+++ signML/screens/PracticeScreen.tsx
@@ -1,21 +1,86 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
+import { MotiView, AnimatePresence } from "moti";
 
 import colors from "../utils/theme";
+import API from "../utils/API";
+
+interface getStateInterface {
+  selectedLetters: string[];
+  selectedIndecies: number[];
+}
+
+interface LetterInterface {
+  letter: string;
+  state: string;
+}
 
 const PracticeScreen = () => {
   const [hasPermission, setHasPermission] = useState<boolean | undefined>();
   const [type, setType] = useState(Camera.Constants.Type.front);
+  const [promptState, setPromptState] = useState<getStateInterface>();
+  const [loading, setLoading] = useState(true);
 
   const requestPerm = async () => {
     const { status } = await Camera.requestCameraPermissionsAsync();
     setHasPermission(status === "granted");
   };
 
+  const fetchPrompt = async () => {
+    return fetch("https://sign-tutor.herokuapp.com/getState")
+      .then((response) => response.json())
+      .then((json) => {
+        setPromptState(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   useEffect(() => {
     requestPerm();
+    fetchPrompt();
   }, []);
+
+  const Letter = ({ letter, state }) => {
+    if (state == 0) {
+      //if the letter has been guessed correctly
+      return (
+        <Text key={letter} style={[styles.baseLetter, styles.falseLetter]}>
+          {letter}
+        </Text>
+      );
+    }
+    if (state == 2) {
+      //if the letter has been guessed correctly
+      return (
+        <MotiView
+          from={{
+            scale: 1.2,
+          }}
+          animate={{
+            scale: 1,
+          }}
+          transition={{
+            loop: true,
+            type: "timing",
+            duration: 1500,
+            delay: 100,
+          }}
+        >
+          <Text key={letter} style={[styles.baseLetter, styles.pendingLetter]}>
+            {letter}
+          </Text>
+        </MotiView>
+      );
+    }
+    return (
+      <Text key={letter} style={[styles.baseLetter, styles.trueLetter]}>
+        {letter}
+      </Text>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -26,8 +91,22 @@ const PracticeScreen = () => {
           <Text>Camera does not have permission, press to open prompt.</Text>
         </TouchableOpacity>
       )}
-      <View style={styles.proptContainer}>
+      <View style={styles.promptContainer}>
         <Text style={{ fontSize: 25, color: "white" }}>PROMPT</Text>
+        <View style={styles.letterContainer}>
+          {promptState ? (
+            promptState.selectedLetters.map((letter, index) => {
+              return (
+                <Letter
+                  letter={letter}
+                  state={promptState.selectedIndecies[index]}
+                />
+              );
+            })
+          ) : (
+            <Text>None found</Text>
+          )}
+        </View>
       </View>
     </View>
   );
@@ -49,13 +128,39 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     overflow: "hidden",
   },
-  proptContainer: {
+  promptContainer: {
     backgroundColor: colors.secondary,
     borderRadius: 20,
-    width: "100%",
-    height: 150,
     marginTop: 50,
     alignItems: "center",
     padding: 8,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 9,
+  },
+  letterContainer: {
+    margin: 25,
+    flexDirection: "row",
+  },
+  baseLetter: {
+    fontSize: 80,
+  },
+  pendingLetter: {
+    color: "white",
+  },
+  falseLetter: {
+    color: "red",
+    fontWeight: "bold",
+  },
+  trueLetter: {
+    color: "green",
+    fontWeight: "bold",
   },
 });
